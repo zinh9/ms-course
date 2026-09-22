b4e9b790560c5a0317edbf79c5f702c648d014fe
zinh9 <enzospadettor@gmail.com>
2026-09-22 20:47:51 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/mscourse/hrpayroll/entities/Worker.java
+++ hr-payroll/src/main/java/com/mscourse/hrpayroll/entities/Worker.java
@@ -1,22 +1,12 @@
-package com.mscourse.hrworker.entities;
+package com.mscourse.hrpayroll.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "tb_worker")
 public class Worker implements Serializable{
 	// para que o objeto pudesse passar na rede
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double dailyIncome;
